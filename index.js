7b5c7cf2e6505d32fe70e8c5bb18025eb1fa1a24
Henry Wu <hhw67865@gmail.com>
2022-10-17 09:02:39 -0400

    commit tree 2091b28069ecfd60cab9e8eda2ba41ae07bdf228

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+function submitData(name, email) {
+       
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email}),
+    })
+    .then(res => res.json())
+    .then(obj => document.body.append(obj.id))
+    .catch(error => document.body.append(error.message));
+}
